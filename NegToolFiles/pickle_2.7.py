44d6bbec97452b481f1f176a7360a6fc358ae0f3
Alan Yuen <alanyuen95@gmail.com>
2018-05-09 11:38:49 -0700

    new meaning spec json

--- NegToolFiles/pickle_2.7.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-f = open('cue_model.pkl','rb')
-
-cue_ssvm = pickle.load(f)
-
-#cue_lexicon = pickle.load(open("cue_lexicon.pkl", "rb"))
--- NegToolFiles/try_pickle.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-favorite_color = { "lion": "yellow", "kitty": "red" }
-
-pickle.dump( favorite_color, open( "save.pkl", "wb" ) )
-    
-favorite_color2 = pickle.load( open( "save.pkl", "rb" ) )
-print(favorite_color2)
